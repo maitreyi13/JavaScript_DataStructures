44437ba9f36a9895e7d277d135b248221ee7d537
Maitreyi Kulkarni <107098811+maitreyi13@users.noreply.github.com>
2022-08-26 22:43:29 +0530

    JavaScript Practice Problems Using Maps

---
+++ Map Problems/birthdays.js
@@ -0,0 +1,29 @@
+/*
+Write a Program to generate a birth month of 50 individual
+between the year 92 & 93. Find all the individuals having birthdays
+in the same month. Store it to finally print.
+*/
+let personCount = 1;
+var birthMonthMap = new Map();
+
+function generateBirthMonth() {
+    return Math.floor(Math.random() * 12) + 1;
+}
+
+while (personCount != 51) {
+    let month = generateBirthMonth();
+    console.log("Person : " + personCount + "\tBirth Month is : " + month);
+    let personArray = new Array();
+
+    if (birthMonthMap.has(month))
+        personArray = birthMonthMap.get(month);
+
+    personArray.push(personCount);
+    birthMonthMap.set(month, personArray);
+    personCount++;
+}
+
+console.log("\nPeople with same birthday month");
+for (let [key, value] of birthMonthMap.entries()) {
+    console.log("Month : " + key + "\tPeople : [" + value + "]");
+}
